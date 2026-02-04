5dc5ea54fa7e67d9fdcd6e1b83281d4721f4be49
Nikunj Goel <kunjgoel2004@gmail.com>
2026-02-04 12:26:54 +0530

    update

--- DBMS.sql
+++ DBMS.sql
@@ -4,4 +4,6 @@ create table students (
 student_id int,
 student_name varchar(255),
 course varchar(255) );
- 
+ insert into students value (001,"Rahul","BCA"),
+ (002,"Neha","MCA");
+ select * from students;
