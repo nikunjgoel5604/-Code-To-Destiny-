03fba3e1a119180b1084ee4f2066e270fa6ca0ff
Nikunj Goel <kunjgoel2004@gmail.com>
2026-02-06 09:42:33 +0530

    update

--- DBMS.sql
+++ DBMS.sql
@@ -1,2 +1,12 @@
 create database collage_db;
 use collage_db;
+create table student(
+student_id int primary key,
+student_Name varchar (255),
+age INT,
+ CHECK(age >= 18) );
+ alter table student 
+ add constraint check_age check(age >=18),
+ add city varchar(255);
+ insert into student values (1,"NIKUNJ",21, "Nodia");
+ select * from student;
