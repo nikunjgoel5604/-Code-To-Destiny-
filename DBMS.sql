cd4b3e04e8044af93d004fbd0ea426e42da57da8
Nikunj Goel <kunjgoel2004@gmail.com>
2026-02-06 09:46:42 +0530

    update

--- DBMS.sql
+++ DBMS.sql
@@ -1,5 +1,5 @@
-create database collage_db;
-use collage_db;
+create database collage_db; -- CREATE DATABASE is used to create a new database
+use collage_db;  -- Select database to work on
 create table student(
 student_id int primary key,
 student_Name varchar (255),
@@ -10,3 +10,4 @@ age INT,
  add city varchar(255);
  insert into student values (1,"NIKUNJ",21, "Nodia");
  select * from student;
+ 
